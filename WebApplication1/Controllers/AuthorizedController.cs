adeef964574e54b89fbe88f5862ca18621fe0559
Axel Schulze <schulze.axel@gmail.com>
2018-04-09 10:50:53 +0200

    Add Authorized Controller

---
+++ WebApplication1/Controllers/AuthorizedController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthorizedController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return Content($"Authorized as {User.Identity.Name}");
+        }
+    }
+}
